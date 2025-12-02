be5cfd843877aee7ff24d4ab80ae1ffe6ab172d3
Lanz <lanzdagamer@gmail.com>
2025-12-02 14:38:28 -0500

    fix format

--- src/List.jsx
+++ src/List.jsx
@@ -2,17 +2,19 @@ import { TodoItem } from "./TodoItem";
 
 export function List({ todos, toggleTodo, deleteTodo }) {
     return (
-        <ul id="list">
-            {todos.map((todo) => {
-                return (
-                    <TodoItem
-                        key={todo.id}
-                        {...todo}
-                        toggleTodo={toggleTodo}
-                        deleteTodo={deleteTodo}
-                    />
-                );
-            })}
-        </ul>
+        <div className="list-container">
+            <ul id="list">
+                {todos.map((todo) => {
+                    return (
+                        <TodoItem
+                            key={todo.id}
+                            {...todo}
+                            toggleTodo={toggleTodo}
+                            deleteTodo={deleteTodo}
+                        />
+                    );
+                })}
+            </ul>
+        </div>
     );
 }
