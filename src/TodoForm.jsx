25f51ebfd01df1a279e5710a26f2120a75747823
Lanz <lanzdagamer@gmail.com>
2025-12-02 14:39:05 -0500

    fix syntax className

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,14 +1,16 @@
 export function TodoForm({ newTodoName, setNewTodoName, addNewTodo }) {
     return (
-        <div id="new-todo-form">
-            <label htmlFor="todo-input">New Todo</label>
+        <form id="new-todo-form" onSubmit={addNewTodo}>
             <input
                 type="text"
                 id="todo-input"
                 value={newTodoName}
                 onChange={(e) => setNewTodoName(e.target.value)}
+                placeholder="What needs to be done?"
             />
-            <button onClick={addNewTodo}>Add Todo</button>
-        </div>
+            <button className="btn-primary" type="submit">
+                Add Task
+            </button>
+        </form>
     );
 }
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -8,11 +8,15 @@ export function TodoItem({ id, name, completed, toggleTodo, deleteTodo }) {
                     data-list-item-checkbox
                     onChange={(e) => toggleTodo(id, e.target.checked)}
                 />
+                <span className="custom-checkbox"></span>
                 <span data-list-item-text>{name}</span>
             </label>
-            <button onClick={() => deleteTodo(id)} data-button-delete>
-                Delete
-            </button>
+            <div className="action-buttons">
+            <button href="#edit-modal" data-button-edit className="btn-icon">Edit</button>
+                <button className="btn-icon danger" onClick={() => deleteTodo(id)} data-button-delete>
+                    Delete
+                </button>
+            </div>
         </li>
     );
 }
