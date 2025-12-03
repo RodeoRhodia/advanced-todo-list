2028fa27f522f6a9e80de949a7d12e17d7f6b1ea
Lanz <lanzdagamer@gmail.com>
2025-12-02 21:08:55 -0500

    add footer info

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { Card } from "./Card";
 import "./styles.css";
 function App() {
-	return <div className="app-wrapper">
-		<Card/>
-	</div>
+    return (
+        <>
+            <div className="app-wrapper">
+                <Card />
+            </div>
+            <div className="footer-credit">
+                By Lanz -{" "}
+                <a
+                    href="https://github.com/RodeoRhodia/advanced-todo-list"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Github
+                </a>
+            </div>
+        </>
+    );
 }
 
 export default App;
