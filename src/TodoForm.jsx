d47cc378cb9b75c91ff3d928467cb0373a075047
Lanz <lanzdagamer@gmail.com>
2025-12-02 20:31:08 -0500

    mobile responsive Add Task btn

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+
 export function TodoForm({ newTodoName, setNewTodoName, addNewTodo }) {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkWidth = () => {
+            setIsMobile(window.innerWidth < 476);
+        };
+        checkWidth();
+        window.addEventListener("resize", checkWidth);
+
+        return () => window.removeEventListener("resize", checkWidth);
+    }, []);
+
     return (
         <form id="new-todo-form" onSubmit={addNewTodo}>
             <input
@@ -9,7 +23,7 @@ export function TodoForm({ newTodoName, setNewTodoName, addNewTodo }) {
                 placeholder="What needs to be done?"
             />
             <button className="btn-primary" type="submit">
-                Add Task
+                {isMobile ? "Add" : "Add Task"}
             </button>
         </form>
     );
